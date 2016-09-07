864c97ed800dfa776b86597dbf58a7478665744b
Shawn Chain <shawn.chain@gmail.com>
2016-09-07 20:51:06 +0800

    Beaconing

--- src/beacon.c
+++ src/beacon.c
@@ -7,11 +7,62 @@
 
 #include "tier2_client.h"
 
+#include <string.h>
+#include <time.h>
+#include "tier2_client.h"
+#include "utils.h"
+#include "config.h"
+
+typedef struct APRSMsg{
+	char type[1];
+	char symbol[2];
+	char lat[8];
+	char lon[9];
+	char text[256];
+}APRSMsg;
+
+// TODO - configurable beacon message
+static char* SRC = "BG5HHP-7";
+static char* DST = "APTI01";
+static APRSMsg aprs = {
+		.type = "!",
+		//.symbol="/r",
+		.symbol="R&",
+		.lat = "3012.48N",
+		.lon = "12008.48E",
+		.text = "431.040MHz iGate/TinyAPRS"
+};
+//#define BEACON_TEXT "BG5HHP-7>APTI01,TCPIP*:!3012.48N/12008.48Er431.040MHz iGate/TinyAPRS\r\n"
+
+static time_t last_beacon = 0;
+
+/*
+ * print APRS message to string
+ */
+static int aprs_print(char* buf, size_t len, APRSMsg* aprs){
+	return snprintf(buf,len,"%c%.8s%c%.9s%c%s",aprs->type[0],aprs->lat,aprs->symbol[0],aprs->lon,aprs->symbol[1],aprs->text);
+}
+
 int beacon_init(){
+	last_beacon = time(NULL) - 300 + 30;
 	return 0;
 }
 
 int beacon_run(){
-	// TODO - broadcast to the network with lat/lon/alt/wx info....
+	time_t t = time(NULL);
+	if(t - last_beacon > (5 * 60)){
+		// BEACONING
+		char payload[1024];
+		int i = sprintf(payload,"%s>%s,TCPIP*:",SRC,DST);
+		i += aprs_print(payload + i,1024 - i - 1,&aprs);
+		payload[i++] = '\r';
+		payload[i++] = '\n';
+		DBG("Beaconing: %s",payload);
+		int rc = tier2_client_publish(payload,i);
+		if(rc < 0){
+			DBG("Beaconing failed, %d",rc);
+		}
+		last_beacon = t;
+	}
 	return 0;
 }
